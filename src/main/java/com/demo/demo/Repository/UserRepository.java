ac63a3448ac2c0295ab8db090edbff4e5187dcb4
Carlos Anoceto <carlosg.anoceto@gmail.com>
2020-04-08 16:59:43 +0100

    Update procfile

--- src/main/java/com/demo/demo/Repository/UserRepository.java
+++ src/main/java/com/demo/demo/Repository/UserRepository.java
@@ -2,8 +2,8 @@ package com.demo.demo.Repository;
 
 import com.demo.demo.Domain.User;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
